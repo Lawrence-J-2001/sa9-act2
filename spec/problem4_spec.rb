cac7deb88fe04e8dee3ddc4194bd6793159c9fae
Lawrence Jones <lawrencejones1234@icloud.com>
2024-03-16 00:08:16 -0500

    Problem 4 rspec, modified

--- spec/problem4_spec.rb
+++ spec/problem4_spec.rb
@@ -1,30 +1,30 @@
 require 'problem4'
 
 RSpec.describe BankAccount do
-    let(:balance) { BankAccount.new(0) }
+    let(:funds) { BankAccount.new(0) }
 
     describe "#deposit" do
     it "increases the balance by the deposit amount" do
-        balance.deposit(250)
-        expect(balance.balance).to eq(250)
+        funds.deposit(250)
+        expect(funds.balance).to eq(250)
     end
     end
 
     describe "#withdaw" do
     it "decreases the balance by the withdrawal amount if funds are available" do
-        balance.deposit(250)
-        balance.withdraw(100)
-        expect(balance.balance).to eq(150)
+        funds.deposit(250)
+        funds.withdraw(100)
+        expect(funds.balance).to eq(150)
     end
     it "does not change the balance if insufficient funds" do
-        balance.withdraw(20)
-        expect(balance.balance).to eq(balance.balance)
+        funds.withdraw(20)
+        expect(funds.balance).to eq(0)
     end
     end
 
     describe "#balance" do
     it "returns the current balance" do
-        expect(balance.balance).to eq(balance.balance)
+        expect(funds.balance).to eq(0)
     end
     end
 end
